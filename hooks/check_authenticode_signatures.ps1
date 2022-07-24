f7eee73c9031f365f39a273e64e129ffb2adcf3d
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-07-23 18:23:37 -0700

    Fix WinError 193: %1 is not a valid Win32 app
    pre-commit requires shebangs on Windows
    https://stackoverflow.com/a/58921968

--- hooks/check_authenticode_signatures.ps1
+++ hooks/check_authenticode_signatures.ps1
@@ -1,9 +1,16 @@
+#!/usr/bin/env powershell
+
 param(
     [string]$Files
 )
 
+$ExitCode = 0
+
 foreach ($File in $Files) {
-    if ($null -ne (Select-String -Pattern '# SIG # Begin signature block' -Path $File)) {
-        exit 1
+    if ($null -ne (Select-String -Pattern '^# SIG # Begin signature block' -Path $File)) {
+        Write-Host "$File contains an Authenticode signature."
+        $ExitCode = 1
     }
 }
+
+exit $ExitCode
